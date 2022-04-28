ba17c4752581b2bc2af5dfd374a77b9e4ec592f9
Paulo Bisetto <100386404+pbisetto@users.noreply.github.com>
2022-04-28 19:34:48 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,13 +1,33 @@
+import java.util.Random;
+
 public class Cliente {
 
     private String nome;
+    private Endereco endereco;
+    private int chavePix;
 
-    public String getNome() {
+    public Cliente() {
+        Random gerador = new Random();
+        this.chavePix= gerador.nextInt(999999999);
+    }
+
+    public String getNome(){
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNome(String nome){
+        this.nome=nome;
     }
 
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getChavePix() {
+        return chavePix;
+    }
 }
